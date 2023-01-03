89fe21fed7b464600416a543adf7c796deb522fa
kk5190 <kk5190krrish@gmail.com>
2023-01-04 02:17:10 +0530

    update cocktail project

--- cocktails/src/pages/About.js
+++ cocktails/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet orci lectus, sed rhoncus velit finibus ac. Cras malesuada eros id metus accumsan, vitae laoreet turpis malesuada. Proin ante dui, vulputate sed massa in, porta pharetra nulla. Integer sodales enim ac sem euismod molestie. Sed sapien elit, condimentum ac porta id, accumsan eu turpis. Cras at commodo lacus, nec tempor nisi. Quisque risus lectus, eleifend in semper at, luctus vel massa. Nam quis dignissim tellus, id congue turpis. Nunc rhoncus orci dui, ut scelerisque nisi molestie eu. Vivamus dictum nibh at massa venenatis convallis. Mauris non accumsan metus, ac venenatis mauris. Aenean facilisis odio sed erat luctus dictum. Sed fermentum nibh at ex ultrices hendrerit.
+      </p>
+    </section>
   )
 }
 
