4b5c67cd5f8bbfc1e0be456008919ba919adfd2c
kk5190 <kk5190krrish@gmail.com>
2023-01-06 18:42:00 +0530

    add cocktail details page

--- cocktails/src/context.js
+++ cocktails/src/context.js
@@ -9,31 +9,32 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('a')
   const [cocktails, setCocktails] = useState([])
 
-  const fetchDrinks = async () => {
-    setLoading(true)
-    try {
-      const response = await fetch(`${url}${searchTerm}`)
-      const data = await response.json()
-      const {drinks} = data
-      if (drinks) {
-        const newCocktails = drinks.map((drink)=>{
-          const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = drink
-          return {id:idDrink, name:strDrink,image:strDrinkThumb,info:strAlcoholic,glass:strGlass}
-        })
-        setCocktails(newCocktails)
-      } else {
-        setCocktails([])
+  const fetchDrinks = useCallback(
+    async () => {
+      setLoading(true)
+      try {
+        const response = await fetch(`${url}${searchTerm}`)
+        const data = await response.json()
+        const {drinks} = data
+        if (drinks) {
+          const newCocktails = drinks.map((drink)=>{
+            const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = drink
+            return {id:idDrink, name:strDrink,image:strDrinkThumb,info:strAlcoholic,glass:strGlass}
+          })
+          setCocktails(newCocktails)
+        } else {
+          setCocktails([])
+        }
+        setLoading(false)
+      } catch(error) {
+        console.log(error)
+        setLoading(false)
       }
-      setLoading(false)
-    } catch(error) {
-      console.log(error)
-      setLoading(false)
-    }
-  }
+    }, [searchTerm])
 
   useEffect(()=>{
     fetchDrinks()
-  }, [searchTerm])
+  }, [searchTerm, fetchDrinks])
 
   return <AppContext.Provider value={{loading, cocktails, setSearchTerm}}>{children}</AppContext.Provider>
 }
