7d9d05cffe3908f8c88131f01e75dc5a155e6f1d
kk5190 <kk5190krrish@gmail.com>
2023-01-28 23:20:52 +0530

    Finished quiz display setup

--- quiz/src/context.js
+++ quiz/src/context.js
@@ -22,7 +22,71 @@ const AppProvider = ({ children }) => {
   const [correct, setCorrect] = useState(0)
   const [error, setError] = useState(false)
 
-  return <AppContext.Provider value='hello'>{ children }</AppContext.Provider>
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const fetchQuestions = async (url) => {
+    setLoading(true)
+    setWaiting(false)
+    const response = await axios(url).catch((err) => console.log(err))
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setQuestions(data)
+        setLoading(false)
+        setWaiting(false)
+        setError(false)
+      } else {
+        setWaiting(true)
+        setError(true)
+      }
+    }else{
+      setWaiting(true)
+    }
+  }
+
+  const nextQuestion = () => {
+    setIndex((oldIndex)=>{
+      const index = oldIndex + 1
+      if (index > questions.length - 1){
+        openModal()
+        return 0
+      }
+      return index
+    })
+  }
+
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((oldState) => oldState + 1)
+    }
+    nextQuestion()
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setWaiting(true)
+    setCorrect(0)
+    setIsModalOpen(false)
+  }
+
+  useEffect(()=>{
+    fetchQuestions(tempUrl)
+  },[])
+
+  return <AppContext.Provider value={{
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    error,
+    isModalOpen,
+    nextQuestion,
+    checkAnswer,
+    closeModal
+  }}>{ children }</AppContext.Provider>
 }
 
 // make sure use
