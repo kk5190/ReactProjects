8cd4fa2d377d5e31dc335da174f0ad0644e880ec
kk5190 <kk5190krrish@gmail.com>
2023-02-08 00:41:13 +0530

    Add featured product section

--- e-commerce/src/context/products_context.js
+++ e-commerce/src/context/products_context.js
@@ -14,13 +14,18 @@ import {
 } from '../actions'
 
 const initialState = {
-  isSidebarOpen: false
+  isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 }
 
 const ProductsContext = React.createContext()
 
 export const ProductsProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
+
   const openSidebar = () => {
     dispatch({type: SIDEBAR_OPEN})
   }
@@ -29,6 +34,21 @@ export const ProductsProvider = ({ children }) => {
     dispatch({type: SIDEBAR_CLOSE})
   }
 
+  const fetchProducts = async (url) => {
+    dispatch({type: GET_PRODUCTS_BEGIN})
+    try {
+      const response = await axios.get(url)
+      const products = response.data
+      dispatch({type: GET_PRODUCTS_SUCCESS, payload: products})
+    } catch (error) {
+      dispatch({type: GET_PRODUCTS_ERROR})
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchProducts(url)
+  },[])
 
   return (
     <ProductsContext.Provider value={{...state, openSidebar, closeSidebar}}>
